277a6b586ab59e21c57f31455412d5396a24c291
Dmitry <gavrilenko6f@gmail.com>
2018-08-01 14:19:33 +0300

    add created and updated time

--- src/main/java/com/mediatype/examplework/model/BaseModel.java
+++ src/main/java/com/mediatype/examplework/model/BaseModel.java
@@ -1,10 +1,14 @@
 package com.mediatype.examplework.model;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @MappedSuperclass
@@ -15,7 +19,31 @@ public class BaseModel {
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     private UUID id;
 
+    @CreationTimestamp
+    @Column(name = "created_time")
+    private LocalDateTime createdTime;
+
+    @UpdateTimestamp
+    @Column(name = "updated_time")
+    private LocalDateTime updatedTime;
+
     public BaseModel() {
+    }
+
+    public LocalDateTime getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(LocalDateTime createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public LocalDateTime getUpdatedTime() {
+        return updatedTime;
+    }
+
+    public void setUpdatedTime(LocalDateTime updatedTime) {
+        this.updatedTime = updatedTime;
     }
 
     public UUID getId() {
