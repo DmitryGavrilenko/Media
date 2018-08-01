02694ae68d34639847dba9c38c982b85eb852d4d
Dmitry <gavrilenko6f@gmail.com>
2018-08-01 13:18:48 +0300

    add module saver

---
+++ data-saver/src/main/java/com/computools/data/saver/Starter.java
@@ -0,0 +1,12 @@
+package com.computools.data.saver;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Starter implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
--- src/test/java/com/mediatype/examplework/ExampleWorkApplicationTests.java
+++ src/test/java/com/mediatype/examplework/ExampleWorkApplicationTests.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
